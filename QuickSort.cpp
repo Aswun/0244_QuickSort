43a0f3cd8cc0ce6aeb57d7624e3c4b04830950b4
Aswun <aswinlutfian32@gmail.com>
2024-03-28 14:20:23 +0700

    membuat input

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -5,3 +5,8 @@ int arr[20]; // array untuk menyimpan nilai
 int cmp_count = 0; // jumlah perbandingan
 int mov_count = 0; // jumlah perpindahan data
 int n; // panjang array
+
+void input() 
+{
+
+}
