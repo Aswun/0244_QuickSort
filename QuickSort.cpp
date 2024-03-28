36b773094e7bd0e2dbd7966392cb9e672be30cff
Aswun <aswinlutfian32@gmail.com>
2024-03-28 14:18:01 +0700

    Deklarasi variabel

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,2 +1,7 @@
 #include <iostream>
 using namespace std;
+
+int arr[20]; // array untuk menyimpan nilai
+int cmp_count = 0; // jumlah perbandingan
+int mov_count = 0; // jumlah perpindahan data
+int n; // panjang array
