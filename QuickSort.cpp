1b57ab15b2784e27b9a139c837d14035b7d2fedb
Aswun <aswinlutfian32@gmail.com>
2024-03-28 14:21:28 +0700

    menambahkan fungsi while pada input

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -8,5 +8,13 @@ int n; // panjang array
 
 void input() 
 {
-
+    while (true)
+    {
+        cout << "Masukan panjang element array: ";
+        cin >> n;
+        if (n <= 20)
+        break;
+            else
+        cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
 }
